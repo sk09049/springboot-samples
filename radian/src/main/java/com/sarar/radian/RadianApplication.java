d55cbf3e9b273341ea7861e165e798be13b8aecc
sara <saravanaonlin@gmail.com>
2020-04-07 17:59:45 +0530

    initial commit

---
+++ radian/src/main/java/com/sarar/radian/RadianApplication.java
@@ -0,0 +1,13 @@
+package com.sarar.radian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RadianApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RadianApplication.class, args);
+	}
+
+}
